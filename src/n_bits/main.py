86c0e19269de6e896d1fdf9bf072c65c86e87dd4
Marc-Antoine Ruel <maruel@gmail.com>
2024-11-04 09:57:19 -0500

    install gnuplot

--- src/n_bits/main.py
+++ src/n_bits/main.py
@@ -152,26 +152,28 @@ def print_bfloat16_components(bfloat16_bytes: bytes):
     exponent_bits = (bfloat16_val >> 7) & 0xFF
     mantissa_bits = bfloat16_val & 0x7F
     print(f"- Binary:   {bfloat16_val:016b} ({bfloat16_val:2x})")
-    print(f"- Sign:     {sign_bit}                ({sign_bit:>3})")
-    print(f"- Exponent:  {exponent_bits:08b}        ({exponent_bits:>3})")
-    print(f"- Mantissa:          {mantissa_bits:07b} ({mantissa_bits:>3})")
+    print(f"- Sign:     {sign_bit}                ({sign_bit:>4})")
+    print(f"- Exponent:  {exponent_bits:08b}        ({exponent_bits:>4})")
+    print(f"- Mantissa:          {mantissa_bits:07b} ({mantissa_bits:>4})")
     print(f"- Decoded:  {decode_bfloat16(bfloat16_val)}")
+
+
+def tensor_stats(t: torch.Tensor):
+    std, mean = torch.std_mean(t)
+    return (t.numel(), mean, std, t.min(), t.max())
 
 
 def inspect_tensors(tensors_dict):
     """Inspect and print information of tensors."""
     # Calculate the stats upfront.
-    stats = {
-        name: (len(f), f.mean(), f.min(), f.max())
-        for name, f in ((name, t.flatten()) for name, t in tensors_dict.items())
-    }
+    stats = {name: tensor_stats(t) for name, t in tensors_dict.items()}
     name_align = max(len(n) for n in tensors_dict)
-    size_align = max(len(str(l)) for l, _, _, _ in stats.values())
-    for name, (length, avg, m1, m2) in stats.items():
+    size_align = max(len(str(s[0])) for s in stats.values())
+    for name, (length, avg, std, m1, m2) in stats.items():
         print(
-            f"{name:<{name_align}}: {length:>{size_align}} items  avg={avg:+.2f} min={m1:+.2f}  max={m2:+.2f}"
+            f"{name:<{name_align}}: len={length:>{size_align}}  avg={avg:+.2f}  std={std:+.2f}  min={m1:+.2f}  max={m2:+.2f}"
         )
-    print(f"- Total number of weights: {sum(l for l, _, _, _ in stats.values())}")
+    print(f"- Total number of weights: {sum(s[0] for s in stats.values())}")
     print(f"- Total number of tensors: {len(tensors_dict)}")
     first_name = next(iter(tensors_dict))
     first = tensors_dict[first_name].flatten()
